cfe6fe030590850a441b0fe05d8ba62aa76423f4
Alex Maina <lexizgichbuoy@gmail.com>
2023-12-07 19:37:05 +0000

    string

--- 0x00-python_variable_annotations/3-to_str.py
+++ 0x00-python_variable_annotations/3-to_str.py
@@ -6,7 +6,7 @@ and returns the string representation of the float.
 
 
 def to_str(n: float) -> str:
-     """
+    """
     Converts a floating-point number to its string representation.
 
     Parameters:
